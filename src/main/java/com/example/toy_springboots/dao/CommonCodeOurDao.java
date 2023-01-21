48d4b4838f3116be99ce994fd8eee5f3d0ffffc8
dwogus6893 <dwogus6893@naver.com>
2023-01-22 01:51:56 +0900

    왜안돼

--- src/main/java/com/example/toy_springboots/dao/CommonCodeOurDao.java
+++ src/main/java/com/example/toy_springboots/dao/CommonCodeOurDao.java
@@ -3,7 +3,6 @@ package com.example.toy_springboots.dao;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
 
 // @Repository
 @Component
