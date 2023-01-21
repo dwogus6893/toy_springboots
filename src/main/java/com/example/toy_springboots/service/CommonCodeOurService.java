5e3954e942e210b85264332b5373afe2b9f9a0db
dwogus6893 <dwogus6893@naver.com>
2023-01-22 00:40:50 +0900

    555

---
+++ src/main/java/com/example/toy_springboots/service/CommonCodeOurService.java
@@ -0,0 +1,59 @@
+package com.example.toy_springboots.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.toy_springboots.dao.CommonCodeOurDao;
+
+
+
+@Service
+public class CommonCodeOurService {
+    @Autowired
+    CommonCodeOurDao commonCodeOurDao;
+
+    // Object dataMap 이것은 파라메타
+    public Object deleteAndGetList(Object dataMap){
+        Object result = this.delete(dataMap);
+        result = this.getList(dataMap);
+        return result;
+    }
+    
+    public Object getList(Object dataMap){
+        String sqlMapId = "CommonCodeOur.selectListByUID";
+        Object result = commonCodeOurDao.getList(sqlMapId, dataMap);
+        return result;
+    }
+
+    public Object getOne(Object dataMap){
+        String sqlMapId = "CommonCodeOur.selectByUID";
+
+        Object result = commonCodeOurDao.getOne(sqlMapId, dataMap);
+        return result;
+    }
+
+    public Object updateOne(Object dataMap){
+        String sqlMapId = "CommonCodeOur.updateByUID";
+
+        Object result = commonCodeOurDao.update(sqlMapId, dataMap);
+        return result;    
+    }
+
+    public Object insertOne(Object dataMap){
+        String sqlMapId = "CommonCodeOur.insertWithUID";
+
+        Object result = commonCodeOurDao.insert(sqlMapId, dataMap);
+        return result;    
+    }
+
+    
+
+    public Object delete(Object dataMap){
+        String sqlMapId = "CommonCodeOur.deleteByUID";
+
+        Object result = commonCodeOurDao.update(sqlMapId, dataMap);
+        return result;    
+    }
+
+    
+    
+}
