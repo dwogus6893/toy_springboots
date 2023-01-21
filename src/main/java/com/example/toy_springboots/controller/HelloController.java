6314395cc7278884bb1690439104eb5610b9f257
dwogus6893 <dwogus6893@naver.com>
2023-01-22 02:10:34 +0900

    ssss

---
+++ src/main/java/com/example/toy_springboots/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.toy_springboots.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String index() {
+        return "Hello, Spring";
+    }
+}
