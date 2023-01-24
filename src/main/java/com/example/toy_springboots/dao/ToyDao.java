b50767de280dcb84acbbc7ebeb238c2347bd7f46
dwogus6893 <dwogus6893@naver.com>
2023-01-25 01:33:08 +0900

    555

--- src/main/java/com/example/toy_springboots/dao/ToyDao.java
+++ src/main/java/com/example/toy_springboots/dao/ToyDao.java
@@ -1,5 +1,15 @@
 package com.example.toy_springboots.dao;
 
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class ToyDao {
-    
+    @Autowired
+    private SqlSession sqlSession;
+
+    public Object getList(String sqlMapId, Object dataMap) {
+        Object result = sqlSession.selectList(sqlMapId, dataMap);
+        return result;
+    }
 }
---
+++ src/main/java/com/example/toy_springboots/service/ToyService.java
@@ -0,0 +1,8 @@
+package com.example.toy_springboots.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ToyService {
+    
+}
